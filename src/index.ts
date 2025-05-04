58ee68ad545e359cd0329e21c223c64600dfd949
Tutitoos <tutitoos.xyz@gmail.com>
2025-05-04 18:59:29 +0200

    feat: add Constants export and enhance Promise interface with catch method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
 
+import Constants from "./Constants";
 import CustomError from "./CustomError";
 import * as utilHttp from "./clients/index";
 
 export default utilHttp;
 export * from "./clients/index";
-export { CustomError };
+export { CustomError, Constants };
+
+// new utilHttp.AxiosClient()
+// 	.get({ url: "https://api.example.com/data" })
+// 	.then((response) => console.log("Response:", response))
+// 	.catch((error) => console.error("Error:", error));
+
+// new utilHttp.AxiosClient()
+// 	.get<Record<string, string>>({ url: "https://pokeapi.co/api/v2/" })
+// 	.then((response) => console.log("Response:", response))
+// 	.catch((error) => console.error("Error:", error));
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+import type CustomError from "./CustomError";
+
 export type Method = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
 export interface ClientOptions {
@@ -11,3 +13,27 @@ export interface ClientOptions {
 export type ContentTypeHandlerKey = "json" | "urlencoded" | "formData" | "text" | "buffer";
 
 export type Client = "axios" | "fetch" | "undici";
+
+declare global {
+	interface Promise<T> {
+		/**
+		 * Attaches callbacks for the resolution and/or rejection of the Promise.
+		 * @param onfulfilled The callback to execute when the Promise is resolved.
+		 * @param onrejected The callback to execute when the Promise is rejected.
+		 * @returns A Promise for the completion of which ever callback is executed.
+		 */
+		then<TResult1 = T, TResult2 = never>(
+			onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
+			onrejected?: ((reason: CustomError) => TResult2 | PromiseLike<TResult2>) | undefined | null
+		): Promise<TResult1 | TResult2>;
+
+		/**
+		 * Attaches a callback for only the rejection of the Promise.
+		 * @param onrejected The callback to execute when the Promise is rejected.
+		 * @returns A Promise for the completion of the callback.
+		 */
+		catch<TResult = never>(
+			onrejected?: ((reason: CustomError) => TResult | PromiseLike<TResult>) | undefined | null
+		): Promise<T | TResult>;
+	}
+}
