439b0c11419a37837e9c7799c99f4614c86b6142
Tutitoos <tutitoos.xyz@gmail.com>
2025-05-04 17:59:43 +0200

    feat: export CustomError from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+import CustomError from "./CustomError";
 import * as utilHttp from "./clients/index";
 
 export default utilHttp;
 export * from "./clients/index";
+export { CustomError };
