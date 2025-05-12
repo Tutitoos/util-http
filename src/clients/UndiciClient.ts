a5fa28669be3570cf7a372ec4a586905432e793f
Tutitoos <tutitoos.xyz@gmail.com>
2025-05-12 16:08:42 +0200

    fix: improve error handling for non-2xx response status in UndiciClient

--- src/clients/UndiciClient.ts
+++ src/clients/UndiciClient.ts
@@ -39,7 +39,7 @@ class UndiciClient {
 			throw HttpClient.handleErrors(error, "undici");
 		});
 
-		if (response.statusCode !== 200) {
+		if (response.statusCode < 200 || response.statusCode > 299) {
 			throw response;
 		}
 
